9e1a84781848c53820390deff43c23acf104fa22
Kristjan Pekk <kristjan.pekk@cyber.ee>
2023-05-16 11:31:34 +0300

    refactoring

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,19 +1,20 @@
 <div class="header">
-    <h1>Game of life</h1>
+  <h1>Game of life</h1>
 </div>
 
 <style>
-    .header {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 15%;
-    }
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 15%;
+  }
 
-    h1 {
-        text-transform: uppercase;
-        font-size: 4rem;
-        font-weight: 100;
-        margin: 0;
-    }
+  h1 {
+    color: var(--primary);
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    margin: 0;
+  }
 </style>
